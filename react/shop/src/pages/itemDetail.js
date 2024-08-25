ad16e30b952d7a8da6973e275e3c75d9bf8e4334
= <ww9720@gamil.com>
2024-08-25 18:26:53 +0900

    use Effect 학습2

--- react/shop/src/pages/itemDetail.js
+++ react/shop/src/pages/itemDetail.js
@@ -15,11 +15,28 @@ let YellowBtn = styled.button`
 
 function ItemDetail(props) {
     let [alertStatus, setAlertStatus] = useState(true);
+    let [inputData, setInputData] = useState('');
+    let [numberAlertStatus, setNumberAlertStatus] = useState(false);
 
+    // useEffect 는 mount, update 시 실행 됨
     useEffect(() => {
         // 해당 부분 코드는 mount, update 시에 실행됨 (렌더링 다 끝나고 동작)
-        setTimeout(() => {setAlertStatus(false)}, 2000)
-    });
+        let timer = setTimeout(() => {setAlertStatus(false)}, 2000);
+
+        if (containsNonNumeric(inputData)) {
+            setNumberAlertStatus(true)
+        } else {
+            setNumberAlertStatus(false)
+        }
+
+        // return() => 문은 useEffect 동작 전에 실행된다
+        return () => {
+            // return 문에서는 아래처럼 기존 코드를 청소(?)하는 코드를 많이 작성한다.
+            clearTimeout(timer);
+        }
+    }, [inputData]);
+    // 뒷부분 []은 useEffect 실행조건 널을 수 있음
+    // 즉, 위 처럼 alertStatus 가 들어가있으면 alertStatus 값이 변경될 때 실행됨
 
     let {itemId} = useParams();
 
@@ -38,18 +55,39 @@ function ItemDetail(props) {
             </div>
             <div className="row">
                 <div className="col-md-6">
-                    <img src={itemImage} width="100%" />
+                    <img src={itemImage} width="100%"/>
                 </div>
                 <div className="col-md-6">
                     <h4 className="pt-5">{itemTitle}</h4>
                     <p>{itemContent}</p>
                     <p>{itemPrice}</p>
+                    <div className="alert alert-danger" role="alert"
+                         style={{display: numberAlertStatus ? "block" : "none"}}>
+                        좋은 말 할 때 숫자만 입력해라
+                    </div>
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="type only number"
+                               aria-label="Recipient's username" aria-describedby="button-addon2"
+                               onChange={(e) => {
+                                   setInputData(e.target.value)
+                               }}/>
+                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                    </div>
                     <YellowBtn bgColor="blue">주문하기</YellowBtn>
                     <YellowBtn bgColor="yellow">주문하기</YellowBtn>
                 </div>
             </div>
         </div>
     )
+}
+
+function containsNonNumeric(value) {
+    // 입력값에서 공백 제거
+    const trimmedValue = value.replace(/\s+/g, '');
+
+    // 숫자 외의 문자 검사
+    const nonNumericPattern = /[^0-9]/;
+    return nonNumericPattern.test(trimmedValue);
 }
 
 export default ItemDetail;
